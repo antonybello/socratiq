48cf60f73606d02d925b982a03dafc95414b7b93
antonybello <antony.bello@pomona.edu>
2017-04-30 15:20:18 -0700

    cleanup article post

--- src/app/components/common/base/renderTextArea.js
+++ src/app/components/common/base/renderTextArea.js
@@ -1,10 +1,10 @@
 import React, { Component, PropTypes } from 'react';
 
 
-const renderField = ({ input, label, type, meta: { touched, error, invalid, warning } }) => (
+const renderField = ({ input, label, className, type, meta: { touched, error, invalid, warning } }) => (
   <div className={`form-group ${touched && invalid ? 'has-error' : ''}`}>
     <div>
-      <textarea {...input} className="form-control"  placeholder={label} type={type}/>
+      <textarea {...input} className={`form-control ${className}`}  placeholder={label} type={type}/>
        <div className="help-block">
       {touched && ((error && <span>{error}</span>) || (warning && <span>{warning}</span>))}
       </div>
